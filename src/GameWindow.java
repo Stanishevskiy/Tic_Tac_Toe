f143f0270ef21a824bfc6d84585a49d7453d0af5
Stanishevskiy <stanishevskiy.a.a@yandex.ru>
2017-04-21 11:12:18 +0300

    add commit

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -5,18 +5,73 @@ import java.awt.event.WindowEvent;
 class GameWindow extends Frame {
 
     GameWindow() {
-        // Отрисовка главного окна
-        setBounds(300, 50, 500, 500);
+        // Отрисовка главного окна и основные свойства
+        setBounds(300, 50, 516, 538);
         setTitle("Tic-Tac-Toe");
-
-        // Переопределяем классы отвечающие за обработку событий своими
         addWindowListener(new MyWindowAdapter());
 
-        Panel genMenuPanel = new Panel();
+        // Панель/карты приложения содержит в себе остальные панели
+        CardLayout cardLayout = new CardLayout();
+        Panel pnlApp = new Panel(cardLayout);
+        add(pnlApp);
 
+        // Добавляем панель главного меню
+        Panel pnlGenMenu = new Panel();
+        pnlGenMenu.setLayout(new GridBagLayout());
+        pnlApp.add(pnlGenMenu, "General Menu");
+        // Создаем ограничитель для компоновщика
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // Название приложения
+        Label lblGenMenu = new Label();
+        lblGenMenu.setText("TIC-TAC-TOE");
+        lblGenMenu.setBackground(Color.LIGHT_GRAY);
+        // Задаем шрифт
+        Font fntLbl = new Font("Helvetica", Font.BOLD, 54);
+        lblGenMenu.setFont(fntLbl);
+        lblGenMenu.setSize(150, 40);
+        // Задаем параметры компоновщика и добавляем на панель
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weighty = 8;
+        pnlGenMenu.add(lblGenMenu, gbc);
+
+        // Задаем кнопки главного меню
+        Button btnStartGame = new Button("Start Game");
+        Button btnSettings = new Button("Settings");
+        Button btnScore = new Button("Score");
+        Button btnExit = new Button("Exit");
+        // Задаем шрифт и расположение кнопок
+        Font fntBtn = new Font("Helvetica", Font.BOLD, 44);
+        btnStartGame.setFont(fntBtn);
+        btnStartGame.setSize(100, 25);
+        btnSettings.setFont(fntBtn);
+        btnSettings.setSize(100, 25);
+        btnScore.setFont(fntBtn);
+        btnScore.setSize(100, 25);
+        btnExit.setFont(fntBtn);
+        btnExit.setSize(100, 25);
+        // Задаем параметры компоновщика и добавляем на панель
+        gbc.gridy = 1;
+        gbc.weighty = 2;
+        gbc.ipadx = 20;
+        pnlGenMenu.add(btnStartGame, gbc);
+        gbc.gridy = 2;
+        gbc.ipadx = 81;
+        pnlGenMenu.add(btnSettings, gbc);
+        gbc.gridy = 3;
+        gbc.ipadx = 132;
+        pnlGenMenu.add(btnScore, gbc);
+        gbc.weighty = 4;
+        gbc.gridy = 4;
+        gbc.ipadx = 172;
+        pnlGenMenu.add(btnExit, gbc);
 
 
         setVisible(true);
+        // Проверка размера панели
+        System.out.println(pnlGenMenu.getWidth());
+        System.out.println(pnlGenMenu.getHeight());
     }
 }
 
