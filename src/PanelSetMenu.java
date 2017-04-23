8300b9d4d70be204c4281e9beada17b3aae15535
Stanishevskiy <stanishevskiy.a.a@yandex.ru>
2017-04-23 10:13:31 +0300

    fix Settings Menu

--- src/PanelSetMenu.java
+++ src/PanelSetMenu.java
@@ -13,150 +13,217 @@ class PanelSetMenu extends Frame implements GamePanelObjects {
         Label lblSettings = new Label("Settings");
         Font fntLblSettings = new Font("Helvetica", Font.BOLD, 34);
         lblSettings.setFont(fntLblSettings);
+        gbc.anchor = GridBagConstraints.CENTER;
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.ipadx = 40;
+        gbc.gridwidth = 2;
         pnlSettings.add(lblSettings, gbc);
 
         // Добавляем составляющие меню
-        Font fntSetElements = new Font("Helvetica", Font.BOLD, 24);
+        Font fntSetElements = new Font("Helvetica", Font.BOLD, 18);
         // Настройки игрока 1
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridwidth = 1;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        Label lblPlayer1 = new Label("Player 1:");
+        gbc.ipadx = 20;
+        Label lblPlayer1 = new Label("Player 1");
         lblPlayer1.setFont(fntSetElements);
         pnlSettings.add(lblPlayer1, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 2;
         Label lblPlayer1Name = new Label("Name: ");
         lblPlayer1Name.setFont(fntSetElements);
         pnlSettings.add(lblPlayer1Name, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 2;
         TextField txtNamePlayer1 = new TextField("Player 1");
         txtNamePlayer1.setFont(fntSetElements);
         pnlSettings.add(txtNamePlayer1, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 3;
         Label lblPlayer1Type = new Label("Player: ");
         lblPlayer1Type.setFont(fntSetElements);
         pnlSettings.add(lblPlayer1Type, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 3;
-        Label chooseTypePlayer1 = new Label("AI, Human");
-        chooseTypePlayer1.setFont(fntSetElements);
-        pnlSettings.add(chooseTypePlayer1, gbc);
+        Choice chsTypePlayer1 = new Choice();
+        chsTypePlayer1.add("Human");
+        chsTypePlayer1.add("AI");
+        chsTypePlayer1.setFont(fntSetElements);
+        pnlSettings.add(chsTypePlayer1, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 4;
         Label lblPlayer1FigureType = new Label("Figure: ");
         lblPlayer1FigureType.setFont(fntSetElements);
         pnlSettings.add(lblPlayer1FigureType, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 4;
-        Label chooseFigureTypePlayer1 = new Label("Cross, Circle, Square");
-        chooseFigureTypePlayer1.setFont(fntSetElements);
-        pnlSettings.add(chooseFigureTypePlayer1, gbc);
+        Choice chsFigureTypePlayer1 = new Choice();
+        chsFigureTypePlayer1.add("Cross");
+        chsFigureTypePlayer1.add("Circle");
+        chsFigureTypePlayer1.add("Square");
+        chsFigureTypePlayer1.add("fCircle");
+        chsFigureTypePlayer1.add("fSquare");
+        chsFigureTypePlayer1.setFont(fntSetElements);
+        pnlSettings.add(chsFigureTypePlayer1, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 5;
         Label lblPlayer1FigureColor = new Label("Color: ");
         lblPlayer1FigureColor.setFont(fntSetElements);
         pnlSettings.add(lblPlayer1FigureColor, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 5;
-        Label chooseFigureColorPlayer1 = new Label("Red, Blue, Yellow, Green, Black");
-        chooseFigureColorPlayer1.setFont(fntSetElements);
-        pnlSettings.add(chooseFigureColorPlayer1, gbc);
+        Choice chsFigureColorPlayer1 = new Choice();
+        chsFigureColorPlayer1.add("Red");
+        chsFigureColorPlayer1.add("Blue");
+        chsFigureColorPlayer1.add("Yellow");
+        chsFigureColorPlayer1.add("Green");
+        chsFigureColorPlayer1.add("Brown");
+        chsFigureColorPlayer1.setFont(fntSetElements);
+        pnlSettings.add(chsFigureColorPlayer1, gbc);
 
         // Настройки игрока 2
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 0;
         gbc.gridy = 6;
-        Label lblPlayer2 = new Label("Player 2:");
+        Label lblPlayer2 = new Label("Player 2");
         lblPlayer2.setFont(fntSetElements);
         pnlSettings.add(lblPlayer2, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 7;
-        Label lblPlayer2Name = new Label("Player: ");
+        Label lblPlayer2Name = new Label("Name: ");
         lblPlayer2Name.setFont(fntSetElements);
         pnlSettings.add(lblPlayer2Name, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 7;
         TextField txtNamePlayer2 = new TextField("Player 2");
         txtNamePlayer2.setFont(fntSetElements);
         pnlSettings.add(txtNamePlayer2, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 8;
         Label lblPlayer2Type = new Label("Player: ");
         lblPlayer2Type.setFont(fntSetElements);
         pnlSettings.add(lblPlayer2Type, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 8;
-        Label chooseTypePlayer2 = new Label("AI, Human");
-        chooseTypePlayer2.setFont(fntSetElements);
-        pnlSettings.add(chooseTypePlayer2, gbc);
+        Choice chsTypePlayer2 = new Choice();
+        chsTypePlayer2.add("AI");
+        chsTypePlayer2.add("Human");
+        chsTypePlayer2.setFont(fntSetElements);
+        pnlSettings.add(chsTypePlayer2, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 9;
         Label lblPlayer2FigureType = new Label("Figure: ");
         lblPlayer2FigureType.setFont(fntSetElements);
         pnlSettings.add(lblPlayer2FigureType, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 9;
-        Label chooseFigureTypePlayer2 = new Label("Cross, Circle, Square");
-        chooseFigureTypePlayer2.setFont(fntSetElements);
-        pnlSettings.add(chooseFigureTypePlayer2, gbc);
+        Choice chsFigureTypePlayer2 = new Choice();
+        chsFigureTypePlayer2.add("Circle");
+        chsFigureTypePlayer2.add("Cross");
+        chsFigureTypePlayer2.add("Square");
+        chsFigureTypePlayer2.add("fCircle");
+        chsFigureTypePlayer2.add("fSquare");
+        chsFigureTypePlayer2.setFont(fntSetElements);
+        pnlSettings.add(chsFigureTypePlayer2, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 10;
         Label lblPlayer2FigureColor = new Label("Color: ");
         lblPlayer2FigureColor.setFont(fntSetElements);
         pnlSettings.add(lblPlayer2FigureColor, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 10;
-        Label chooseFigureColorPlayer2 = new Label("Red, Blue, Yellow, Green, Black");
-        chooseFigureColorPlayer2.setFont(fntSetElements);
-        pnlSettings.add(chooseFigureColorPlayer2, gbc);
+        Choice chsFigureColorPlayer2 = new Choice();
+        chsFigureColorPlayer2.add("Blue");
+        chsFigureColorPlayer2.add("Red");
+        chsFigureColorPlayer2.add("Yellow");
+        chsFigureColorPlayer2.add("Green");
+        chsFigureColorPlayer2.add("Brown");
+        chsFigureColorPlayer2.setFont(fntSetElements);
+        pnlSettings.add(chsFigureColorPlayer2, gbc);
 
         // Игровые параметры
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 11;
         Label lblGameFieldSize = new Label("Field Size: ");
         lblGameFieldSize.setFont(fntSetElements);
         pnlSettings.add(lblGameFieldSize, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 11;
-        Label chooseGameFieldSize = new Label("3...9");
-        chooseGameFieldSize.setFont(fntSetElements);
-        pnlSettings.add(chooseGameFieldSize, gbc);
+        Choice chsGameFieldSize = new Choice();
+        chsGameFieldSize.add("3");
+        chsGameFieldSize.add("4");
+        chsGameFieldSize.add("5");
+        chsGameFieldSize.add("6");
+        chsGameFieldSize.add("7");
+        chsGameFieldSize.add("8");
+        chsGameFieldSize.add("9");
+        chsGameFieldSize.setFont(fntSetElements);
+        pnlSettings.add(chsGameFieldSize, gbc);
 
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.gridx = 0;
         gbc.gridy = 12;
-        Label lblCombToWin = new Label("Winning combination: ");
+        Label lblCombToWin = new Label("Combo to Win: ");
         lblCombToWin.setFont(fntSetElements);
         pnlSettings.add(lblCombToWin, gbc);
 
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 1;
         gbc.gridy = 12;
-        Label chooseCombToWin = new Label("3...9");
-        chooseCombToWin.setFont(fntSetElements);
-        pnlSettings.add(chooseCombToWin, gbc);
+        Choice chsCombToWin = new Choice();
+        chsCombToWin.add("3");
+        chsCombToWin.add("4");
+        chsCombToWin.add("5");
+        chsCombToWin.add("6");
+        chsCombToWin.add("7");
+        chsCombToWin.add("8");
+        chsCombToWin.add("9");
+        chsCombToWin.setFont(fntSetElements);
+        pnlSettings.add(chsCombToWin, gbc);
 
         // Добавляем кнопку выхода из меню настроек
+        gbc.anchor = GridBagConstraints.CENTER;
         gbc.gridx = 0;
         gbc.gridy = 13;
+        gbc.gridwidth = 2;
         Button btnToGenMenu = new Button("Back To Menu");
         btnToGenMenu.setFont(fntSetElements);
         pnlSettings.add(btnToGenMenu, gbc);
